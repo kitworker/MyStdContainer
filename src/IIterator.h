3a481d1ccbe93cbeafaddc7c37cc737565910255
knik <kasyanov.nn@gmail.com>
2018-02-13 21:12:07 +0300

    created IteratorBack

--- src/IIterator.h
+++ src/IIterator.h
@@ -1,19 +1,35 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
-class IIterator {
+class Accessor {
 public:
-    virtual ~IIterator();
-    virtual void First() = 0;
-    virtual void Next() = 0;
-    virtual bool IsDone() const = 0;
     template<class Item>
     Item * CurrentItem() const {
     	assert(!"Incorrect overridden");
     	return NULL;
     }
+};
+
+class IIterator : public Accessor {
+public:
+    virtual ~IIterator();
+    virtual void First() = 0;
+    virtual void Next() = 0;
+    virtual bool IsDone() const = 0;
+
 protected:
     IIterator();
+};
+
+class IIteratorBack : public Accessor {
+public:
+    virtual ~IIteratorBack() {}
+    virtual void End() = 0;
+    virtual void Prev() = 0;
+    virtual bool IsHead() = 0;
+
+protected:
+    IIteratorBack() {}
 };
 
 IIterator::IIterator() {
--- src/Iterator.h
+++ src/Iterator.h
@@ -31,6 +31,29 @@ private:
 	typename ContainerStl<TData, TContain>::iterator curr;
 };
 
+template<typename TData, template<typename _Tp,	typename _Alloc = std::allocator<_Tp> >
+		 class TContain>
+class IteratorBack: public IIteratorBack{
+	//typename IteratorBack<TData, TContain>::reverse_iterator rCurr;
+	typedef typename TContain<TData>::reverse_iterator reverse_iterator;
+	reverse_iterator rCurr;
+	IteratorBack<TData, TContain> & container;
+public:
+	virtual ~IteratorBack() {
+	}
+
+	IteratorBack(ContainerStl<TData, TContain> &  c) :
+			container(c), rCurr(c.Begin()) {
+	}
+
+	// By GoF
+	virtual void First();
+	virtual void Next();
+	virtual bool IsDone() const;
+	const TData& CurrentItem() const;
+
+};
+
 #include "Iterator.hpp"
 
 #endif /* ITERATOR_H_ */
